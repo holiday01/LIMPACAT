f7758feab57d0a8581fb613f0c37e6091c0a8083
holiday <zx12as3420@gmail.com>
2024-11-02 11:28:34 +0800

    Update docker_env_set.sh

--- docker_env_set.sh
+++ docker_env_set.sh
@@ -8,3 +8,15 @@ ExecStart=/usr/bin/dockerd --host=fd:// --add-runtime=nvidia=/usr/bin/nvidia-con
 EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
+
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+&& curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+&& curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+# 更新 apt repository 並安裝 NVIDIA Container Toolkit
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+# 重新啟動 Docker 以使更改生效
+sudo systemctl restart docker
